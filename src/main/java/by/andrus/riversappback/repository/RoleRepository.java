59e39105729db4ee729bc9667dc4c2a7f6942e15
AndrusKr <andrus.kryvicenka@gmail.com>
2020-04-24 14:12:57 +0300

    @Repository added

--- src/main/java/by/andrus/riversappback/repository/RoleRepository.java
+++ src/main/java/by/andrus/riversappback/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package by.andrus.riversappback.repository;
 
 import by.andrus.riversappback.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     List<Role> findAll();
 }
--- src/main/java/by/andrus/riversappback/repository/UserRepository.java
+++ src/main/java/by/andrus/riversappback/repository/UserRepository.java
@@ -2,7 +2,9 @@ package by.andrus.riversappback.repository;
 
 import by.andrus.riversappback.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     void deleteById(Long id);
