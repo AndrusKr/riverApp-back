316c83aac94249499d6207292c525f355cd9f3b1
AndrusKr <andrus.kryvicenka@gmail.com>
2020-04-24 15:10:31 +0300

    Auditable functionality columns explicitly added to DB

--- src/main/resources/db/migration/V1__Create_users_table.sql
+++ src/main/resources/db/migration/V1__Create_users_table.sql
@@ -6,10 +6,14 @@ CREATE TABLE users
     first_name VARCHAR(100)                                 NOT NULL,
     last_name  VARCHAR(100)                                 NOT NULL,
     password   VARCHAR(255)                                 NOT NULL,
-    created    TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()    NOT NULL,
-    updated    TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()    NOT NULL,
+    created_by BIGINT                                       NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()    NOT NULL,
+    updated_by BIGINT                                       NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()    NOT NULL,
     status     VARCHAR(25)                 DEFAULT 'ACTIVE' NOT NULL,
     CONSTRAINT USERS_PKEY PRIMARY KEY (id),
+    FOREIGN KEY (created_by) REFERENCES users (id),
+    FOREIGN KEY (updated_by) REFERENCES users (id),
     UNIQUE (email),
     UNIQUE (username)
 );
